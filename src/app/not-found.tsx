4fefe1c2b237ad7e562aad14ceb25a1f7caa9b43
Charles <charleszecevic@gmail.com>
2024-07-04 15:46:07 +0200

    elem

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,12 +7,12 @@ export default function Custom404() {
       {/* <ParticlesBackground /> */}
       <div className="text-center flex flex-col">
         <h1 className="text-6xl font-bold">404</h1>
-        <p className="text-2xl mt-4">Oops! pas trouvé :(</p>
+        <p className="text-2xl mt-4">Oops! pas trouvé :&#91;</p>
         <Link
           href="/"
           className="mt-6 text-lg text-black bg-yellow-500 rounded-lg p-5 transition duration-300 ease-in-out hover:bg-yellow-100"
         >
-          Retourner à l'accueil
+          Retourner à l&#39;accueil
         </Link>
       </div>
     </div>
